ae47a7e09edcfe81ba8e87c5e2762ffc49ff11a8
shimanta <sarkershimanta@gmail.com>
2025-04-14 20:37:24 +0600

    counter app completed

--- flutter_bloc_counter/lib/ui/counter_screen.dart
+++ flutter_bloc_counter/lib/ui/counter_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_counter/bloc/counter.dart';
+import 'package:flutter_bloc_counter/bloc/counter_block.dart';
+import 'package:flutter_bloc_counter/bloc/counter_state.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -11,20 +15,35 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("counter"),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Icon(Icons.plus_one), Icon(Icons.person)],
-      ),
-      body: Center(
-        child: Text(
-          "counter",
-          style: TextStyle(fontSize: 30),
+        appBar: AppBar(
+          title: Text("counter"),
         ),
-      ),
-    );
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                context.read<CounterBlock>().add(IncrementCounter());
+              },
+              child: Icon(Icons.plus_one),
+            ),
+            InkWell(
+              onTap: () {
+                context.read<CounterBlock>().add(DecrementCounter());
+              },
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body:
+            BlocBuilder<CounterBlock, CounterState>(builder: (context, state) {
+          return Center(
+            child: Text(
+              "${state.counter}",
+              style: TextStyle(fontSize: 30),
+            ),
+          );
+        }));
   }
 }
